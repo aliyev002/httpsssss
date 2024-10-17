380aba2b9a80ded0241ba596e206e7e464f3846e
Aliyev Togrul Ceyhun <Aliye_du26@itstep.edu.az>
2024-10-17 17:35:08 +0400

    first commit

---
+++ Context/UserContext.cs
@@ -0,0 +1,27 @@
+﻿using HttpRequestDB.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequestDB.Context
+{
+    internal class UserContext
+    {
+        public class BusContext : DbContext
+        {
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source = STHQ0118-07;User ID=admin;Password=admin; Database=Users;TrustServerCertificate=True");
+            }
+            public DbSet<User> Users { get; set; }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<User>().ToTable("users");
+            }
+        }
+    }
+}
---
+++ Service/Methods.cs
@@ -0,0 +1,21 @@
+﻿using HttpRequestDB.Context;
+using HttpRequestDB.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpRequestDB.Service
+{
+    public class Methods
+    {
+        public List<User> GetAllUsers()
+        {
+            using (var context = new UserContext())
+            {
+                return context.Users.ToList();
+            }
+        }
+    }
+}
